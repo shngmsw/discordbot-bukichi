8557e54b2bdc199349061fd69ce57df9d6ea3c76
shngmsw <mose1021@gmail.com>
2022-11-23 16:00:09 +0900

    fix:dispandar

--- app/event/dispandar.js
+++ app/event/dispandar.js
@@ -1,26 +1,46 @@
-const Discord = require('discord.js');
-const regexDiscrdMessageUrl =
-    'https://(ptb.|canary.)?discord(app)?.com/channels/' + '(?<guild>[0-9]{18})/(?<channel>[0-9]{18})/(?<message>[0-9]{18})';
+const common = require('../common');
+const log4js = require('log4js');
+
+log4js.configure(process.env.LOG4JS_CONFIG_PATH);
+const logger = log4js.getLogger('dispander');
+
+const regexDiscordMessageUrl =
+    'https://(ptb.|canary.)?discord(app)?.com/channels/' + '(?<guild>[0-9]{18,19})/(?<channel>[0-9]{18,19})/(?<message>[0-9]{18,19})';
 
 module.exports = {
     dispand: dispand,
 };
 
 async function dispand(message) {
-    messages = await extractMessages(message);
-    for (var m in messages) {
-        if (message.content) {
-            await message.channel.send(composeEmbed(messages[m]));
+    try {
+        var messages = await extractMessages(message);
+        var url;
+
+        if (common.isNotEmpty(messages)) {
+            for (var m in messages) {
+                if (message.content) {
+                    url = message.content.match(regexDiscordMessageUrl);
+                    await message.channel.send({
+                        embeds: [await common.composeEmbed(messages[m], url[0])],
+                    });
+                }
+                for (var embed in messages[m].embeds) {
+                    await message.channel.send({ embeds: [messages[m].embeds[embed]] });
+                }
+                if (message.content === url[0]) {
+                    message.delete();
+                }
+            }
         }
-        for (var embed in messages[m].embeds) {
-            await message.channel.send(messages[m].embeds[embed]);
-        }
+    } catch (error) {
+        logger.error(error);
+        message.reply('なんかエラー出てるわ');
     }
 }
 
 async function extractMessages(message) {
     let messages = new Array();
-    let matches = message.content.match(regexDiscrdMessageUrl);
+    let matches = message.content.match(regexDiscordMessageUrl);
     if (!matches) {
         return;
     }
@@ -28,24 +48,50 @@ async function extractMessages(message) {
     if (guild.id != matches.groups.guild) {
         return;
     }
-    fetchedMessage = await fetchMessageFromId(guild, matches.groups.channel, matches.groups.message);
-    messages.push(fetchedMessage);
+    const fetchedMessage = await searchMessageById(guild, matches.groups.channel, matches.groups.message);
+    if (common.isNotEmpty(fetchedMessage)) {
+        messages.push(fetchedMessage);
+    } else {
+        message.reply('メッセージが見つからなかったでし！');
+        return;
+    }
+
     return messages;
 }
 
-async function fetchMessageFromId(guild, chId, msgId) {
-    let channel = guild.channels.cache.find((channel) => channel.id === chId);
-    return channel.messages.fetch(msgId);
+/**
+ * メッセージIDからメッセージを検索する．ない場合はnullを返す．
+ * @param {string} guild Guildオブジェクト
+ * @param {string} channelId チャンネルID
+ * @param {string} messageId メッセージID
+ * @returns メッセージオブジェクト
+ */
+async function searchMessageById(guild, channelId, messageId) {
+    const channel = await searchChannelById(guild, channelId);
+    let message = null;
+    if (channel) {
+        try {
+            message = await channel.messages.fetch({ message: messageId });
+        } catch (error) {
+            logger.warn('message missing');
+        }
+    }
+    return message;
 }
 
-function composeEmbed(message) {
-    const embed = new Discord.MessageEmbed();
-    embed.setDescription(message.content);
-    embed.setTimestamp(message.createdAt);
-    embed.setAuthor((name = message.author.username), (iconURL = message.author.avatarURL()));
-    embed.setFooter((text = message.channel.name), (iconURL = message.guild.iconURL()));
-    if (message.attachments.size > 0 && message.attachments[0].proxyURL) {
-        embed.setImage(message.message.attachments[0].proxyURL);
+/**
+ * チャンネルIDからチャンネルを検索する．ない場合はnullを返す．
+ * @param {string} guild Guildオブジェクト
+ * @param {string} channelId チャンネルID
+ * @param {string} categoryId カテゴリID or null
+ * @returns チャンネルオブジェクト
+ */
+async function searchChannelById(guild, channelId) {
+    let channel = null;
+    try {
+        channel = await guild.channels.fetch(channelId);
+    } catch (error) {
+        logger.warn('channel missing');
     }
-    return embed;
+    return channel;
 }
